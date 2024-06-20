e5008eebc57f381c628762bfcdf5d0f7e8d3e021
mptsolutioning <167113569+mptsolutioning@users.noreply.github.com>
2024-06-20 16:12:00 -0700

    waf v2

--- main.tf
+++ main.tf
@@ -161,6 +161,29 @@ resource "aws_acm_certificate" "frontend_cert" {
   }
 }
 
+resource "aws_route53_record" "dns_validation" {
+
+  for_each = {
+    for dvo in aws_acm_certificate.frontend_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.frontend_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.dns_validation : record.fqdn]
+}
+
 resource "aws_cloudfront_origin_access_control" "s3_bucket_static_website" {
   name                              = "Managed-S3OriginPolicy"
   description                       = "Managed policy for S3 origin access"
@@ -333,7 +356,7 @@ resource "aws_cloudfront_distribution" "frontend" {
     }
   }
 
-  depends_on = [aws_acm_certificate.frontend_cert]
+  depends_on = [aws_acm_certificate_validation.cert_validation]
 }
 
 resource "aws_route53_record" "frontend" {
@@ -346,4 +369,6 @@ resource "aws_route53_record" "frontend" {
     zone_id                = aws_cloudfront_distribution.frontend.hosted_zone_id
     evaluate_target_health = false
   }
+
+  depends_on = [aws_cloudfront_distribution.frontend]
 }
